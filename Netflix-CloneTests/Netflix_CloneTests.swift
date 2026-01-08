f01ae049f4807f7702bcb1cfc4e397c1db6612d6
Ritika <bhardwajritika11@gmail.com>
2026-01-09 02:54:08 +0530

    Initial Commit

---
+++ Netflix-CloneTests/Netflix_CloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  Netflix_CloneTests.swift
+//  Netflix-CloneTests
+//
+//  Created by Tarun Sharma on 09/01/26.
+//
+
+import Testing
+@testable import Netflix_Clone
+
+struct Netflix_CloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
