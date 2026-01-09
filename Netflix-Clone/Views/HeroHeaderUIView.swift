4365e3a4d77c910a6f376f32288f200d2c0ab052
Ritika <bhardwajritika11@gmail.com>
2026-01-10 02:01:12 +0530

    hero header image done

---
+++ Netflix-Clone/Views/HeroHeaderUIView.swift
@@ -0,0 +1,43 @@
+//
+//  HeroHeaderUIView.swift
+//  Netflix-Clone
+//
+//  Created by Tarun Sharma on 09/01/26.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+    
+    private let heroImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "heroImage")
+        return imageView
+    }()
+    
+    private func addGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor.clear.cgColor,
+            UIColor.systemBackground.cgColor
+        ]
+        layer.addSublayer(gradientLayer)
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(heroImageView)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        heroImageView.frame = bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+
+}
